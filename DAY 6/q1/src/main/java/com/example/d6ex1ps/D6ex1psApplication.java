24700fed900c52c48fb5725f7546b451e5105334
Yalinibalasubramani <121392933+Yalinibalasubramani@users.noreply.github.com>
2024-04-24 09:37:13 +0530

    Add files via upload

---
+++ DAY 6/q1/src/main/java/com/example/d6ex1ps/D6ex1psApplication.java
@@ -0,0 +1,13 @@
+package com.example.d6ex1ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D6ex1psApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D6ex1psApplication.class, args);
+	}
+
+}
---
+++ DAY 6/q1/src/test/java/com/example/d6ex1ps/D6ex1psApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d6ex1ps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D6ex1psApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
